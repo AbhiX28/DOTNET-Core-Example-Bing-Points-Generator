26570b4ea1c58bac3dd5e41524e74a750e0c5fca
Abhishek Bagga <Abhishek.Bagga@reedbusiness.com>
2020-05-20 18:55:58 +0100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using System.Diagnostics;
+using System.Threading;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+
+namespace BingPointsNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Directory.GetCurrentDirectory());
+            string fullPtah = Path.GetDirectoryName(new Uri(System.Reflection.Assembly.GetExecutingAssembly().CodeBase).LocalPath);
+            Console.WriteLine(Path.GetRelativePath("BingPointsNetCore", fullPtah));
+            //Console.WriteLine(Figgle.FiggleFonts.Standard.Render("Hello, World!"));
+
+            executeSearch();
+        }
+
+        static void executeSearch()
+        {
+            string filePath = "phrases.txt";
+            //Get List of phrases from File
+            List<String> phrases = new List<string>(File.ReadAllLines(filePath));
+            string url = "https://account.microsoft.com/rewards/";
+            openURL(url);
+            Thread.Sleep(3000);
+            for (int i = 0; i < 30; i++)
+            {
+                Random rnd = new Random();
+                int index = rnd.Next(phrases.Count);
+                string strPhrase = phrases[index];
+                phrases.RemoveAt(index);
+                url = "https://www.bing.com/search?q=" + strPhrase;
+                openURL(url);
+                Thread.Sleep(3000);
+            }
+            url = "https://account.microsoft.com/rewards/";
+            openURL(url);
+
+            //if you want to update the contents of the txt file 
+            //phrases.ForEach(Console.WriteLine);
+            //File.WriteAllLines(filePath, phrases);
+        }
+
+        static void openURL(string url)
+        {
+            // Good Link for Reference using Process in Multiple Platforms
+            // https://brockallen.com/2016/09/24/process-start-for-urls-on-net-core/
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                url = url.Replace("&", "^&");
+                Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = false });
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                Process.Start("open", url);
+            }
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# Bing Points Generator
+
+Started as Hobby project to test .Net Core during it's initial launch to Test the functionlaity on MacOS & Win Platforms
+Further extended it to Search on various Phrases and expanding the Vocabulary, at the same time Generate & add Bing Points to your account
+
+## Geting up and running (Runs on Both Win & MacOS)
+- dotnet build
+- dotnet run
+
+## Saving Bing Points
+- Make sure you are logged in to your MS account on the default browser
+
+## Caution 
+- The program opens multiple Browser Tabs, you can adjust the timings between each new tab opening, Program.cs line number 39 to increase the sleep timer
